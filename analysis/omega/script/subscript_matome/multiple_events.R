39abebcc701f72a7262fde4207623d30eb86c909
friend1ws <friend1ws@gmail.com>
2016-11-03 23:18:02 +0900

    modify multiple_events.R script to draw #splicing by multiple mutations

--- analysis/omega/script/subscript_matome/multiple_events.R
+++ analysis/omega/script/subscript_matome/multiple_events.R
@@ -24,3 +24,25 @@ ggplot(multiple_effect_count, aes(x = splice_count, y = count)) +
 
 ggsave("../matome/multi_event.png", width = 6, height = 6)
 
+
+splicing_multiple_mut <- splicing_mutation %>% 
+  select(Mutation_Key, Splicing_Key, Gene_Symbol, Splicing_Class, Is_Inframe) %>% 
+  distinct() %>%
+  group_by(Splicing_Key, Gene_Symbol, Splicing_Class, Is_Inframe) %>%
+  summarize(mut_count = n()) 
+
+splicing_multiple_mut_count <- splicing_multiple_mut %>%
+  group_by(mut_count) %>% 
+  summarize(count = n())
+
+
+ggplot(splicing_multiple_mut_count, aes(x = mut_count, y = count)) + 
+  geom_bar(stat = "identity") + 
+  labs(x = "#associated mutations", y = "#splicing events") +
+  scale_y_log10() +
+  theme_minimal() +
+  theme(axis.text = element_text(size = rel(1.2)),
+        axis.title = element_text(size = rel(1.2)))
+
+ggsave("../matome/multi_event_sp.png", width = 6, height = 6)
+
