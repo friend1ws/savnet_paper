7e2989e02b30337a1f16f064ee7915ca9835458e
friend1ws <friend1ws@gmail.com>
2016-12-21 11:16:05 +0900

    organize JungEtAl comparison scripts

--- analysis/omega/script/runall2.sh
+++ analysis/omega/script/runall2.sh
@@ -6,13 +6,17 @@ then
 fi
 
 
-python subscript_matome/matome_omega.py ../output ../matome/omega.genomon_splicing_mutation.result.txt /home/yshira/mysoftware/sv_utils/cancer_gene/cancer_gene.txt 3 0.10 
+python subscript_matome/matome_omega.py ../gsm_out/d3.6_a6.1_8_ka ../matome/omega.genomon_splicing_mutation.result.txt /home/yshira/mysoftware/sv_utils/cancer_gene/cancer_gene.txt ../HGMD/HGMD.CS.bed.gz 3 0.10 
 
-python subscript_matome/summarize_motif_pos.py ../matome/omega.genomon_splicing_mutation.result.txt ../matome/omega.motif_summary.txt /home/w3varann/database/GRCh37/GRCh37.fa
+python subscript_matome/summarize_snv_info.py  ../matome/omega.genomon_splicing_mutation.result.txt ../matome/omega.motif_summary.txt /home/w3varann/database/GRCh37/GRCh37.fa ../spidex/hg19_spidex.bed.gz
+
+Rscript subscript_matome/add_mes_score.R ../matome/omega.motif_summary.txt ../matome/omega.motif_summary.mes.txt
 
 python subscript_matome/check_inframe.py ../matome/omega.genomon_splicing_mutation.result.txt ../matome/gene.inframe_summary.txt
 
 python subscript_matome/summarize_mut_count.py ../matome/omega.mut_count.txt
+
+python subscript_matome/check_position_fdr.py ../gsm_out/d5.15_a15.5_8_ka/ 3.0 > ../matome/position_fdr.txt
 
 Rscript subscript_matome/category2.R 
 
@@ -23,6 +27,11 @@ Rscript subscript_matome/multiple_events.R
 Rscript subscript_matome/count_summary.R
 
 Rscript subscript_matome/cancer_gene_summary.R 
+
+Rscript subscript_matome/mes_hbond_spidex.fig.R
+
+Rscript subscript_matome/JungEtAl_venn.R
+
 
 <<_COM_
 
